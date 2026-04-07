cc68da8cabbcf814d6a8b4322574ed1ed179c447
Balaji Bathini <balajijr007@gmail.com>
2026-04-07 18:41:16 +0530

    add company favicon customization and update related views

---
+++ bb_web_company_favicon_logo/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Customize Company Backend web icon',
+    'version': '19.0.0.1',
+    'category': 'Extra Tools',
+    'sequence': 20,
+    'author': 'BB Logic',
+    'summary': 'The custom web window Favicon',
+    'description': """
+Customize Favicon
+==============================================
+    """,
+    'depends': ['web'],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'website': '',
+    'installable': True,
+    'auto_install': False,
+    "application": True,
+    'license': 'LGPL-3',
+    'assets': {
+        'web.assets_backend': [
+            'bb_web_company_favicon_logo/static/src/js/favicon.js',
+        ],
+    },
+    'qweb': [],
+}
